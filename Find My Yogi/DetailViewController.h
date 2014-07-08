71c6acda1c64e612b5ee260b80720c1d24e4b557
Jeff Berman <spamact@yahoo.com>
2014-07-08 15:41:40 -0700

    Add items to profile scene

--- Find My Yogi/DetailViewController.h
+++ Find My Yogi/DetailViewController.h
@@ -7,10 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UserFeedItem.h"
 
 @interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
 
-@property (strong, nonatomic) id detailItem;
+@property (strong, nonatomic) UserFeedItem *detailItem;
 
-@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@property (getter = isYogi) BOOL yogi; // Temp? -- am I a yogi
+
+@property (weak, nonatomic) IBOutlet UIImageView *profilePhotoImage;
+@property (weak, nonatomic) IBOutlet UILabel *shortBioLabel;
+@property (weak, nonatomic) IBOutlet UIButton *followButton;
+@property (weak, nonatomic) IBOutlet UIButton *followersButton;
+@property (weak, nonatomic) IBOutlet UIButton *instagramButton;
 @end
