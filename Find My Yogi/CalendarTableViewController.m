77ea543f054fa422eedc8f5908bb8a59947a31e4
Jeff Berman <spamact@yahoo.com>
2014-07-14 22:47:14 -0700

    Added reuse identifier to calendar cell.

--- Find My Yogi/CalendarTableViewController.m
+++ Find My Yogi/CalendarTableViewController.m
@@ -60,7 +60,7 @@
 /*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CalendarCell" forIndexPath:indexPath];
     
     // Configure the cell...
     
